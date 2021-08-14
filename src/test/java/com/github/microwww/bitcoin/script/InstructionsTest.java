b2eb440c5947e0fcdbaaf5d047a758c3f8e24d89
changshu.li <lcs.005@163.com>
2021-08-14 13:03:45 +0800

    scriptName test

--- src/test/java/com/github/microwww/bitcoin/script/InstructionsTest.java
+++ src/test/java/com/github/microwww/bitcoin/script/InstructionsTest.java
@@ -18,4 +18,16 @@ class InstructionsTest {
         assertEquals(Instructions.SET.select(175).toString(), "OP_CHECKMULTISIGVERIFY");
         assertEquals(Instructions.SET.select(186).toString(), "_186");
     }
+
+    @Test
+    public void scriptName() {
+        assertEquals(ScriptNames.values()[75].name(), "_75");
+        assertEquals(ScriptNames.values()[96].name(), "OP_16");
+        assertEquals(ScriptNames.values()[115].name(), "OP_IFDUP");
+        assertEquals(ScriptNames.values()[125].name(), "OP_TUCK");
+        assertEquals(ScriptNames.values()[137], ScriptNames.OP_RESERVED1);
+        assertEquals(ScriptNames.values()[160].name(), "OP_GREATERTHAN");
+        assertEquals(ScriptNames.values()[175].name(), "OP_CHECKMULTISIGVERIFY");
+        assertEquals(ScriptNames.values()[186].name(), "_186");
+    }
 }
